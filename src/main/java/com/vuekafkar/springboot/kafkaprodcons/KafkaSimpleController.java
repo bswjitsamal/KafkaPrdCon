5bbcbc4b84d221257f4e9083a410dee90ae3fee7
bswjitsamal@gmail.com <Biswa@1984>
2025-07-02 12:33:42 +0530

    3rd commit.

--- src/main/java/com/vuekafkar/springboot/kafkaprodcons/KafkaSimpleController.java
+++ src/main/java/com/vuekafkar/springboot/kafkaprodcons/KafkaSimpleController.java
@@ -2,12 +2,18 @@ package com.vuekafkar.springboot.kafkaprodcons;
 
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/kafka")
@@ -26,15 +32,16 @@ public class KafkaSimpleController {
      */
 
     @PostMapping
-    public void post(@RequestBody SimpleModel simpleModel){
-        kafkaTemplate.send("myTopic", jsonConverter.toJson(simpleModel));
+    public ResponseEntity<String> post(@RequestBody SimpleModel simpleModel) throws InterruptedException, ExecutionException {
+        CompletableFuture<SendResult<String, String>> result = kafkaTemplate.send("myTopic", jsonConverter.toJson(simpleModel));
+        return new ResponseEntity<>(result.get().getProducerRecord().value(), HttpStatus.OK);
     }
 
     @KafkaListener(topics = "myTopic")
     public void getFromKafka(String simpleModel){
-        System.out.println(simpleModel);
+        System.out.println("Kafka event consumed is: " + simpleModel);
         SimpleModel simpleModel1 = jsonConverter.fromJson(simpleModel, SimpleModel.class);
-        System.out.println(simpleModel1.toString());
+        System.out.println("Model converted value: " + simpleModel1.toString());
     }
 
     /**
@@ -42,15 +49,15 @@ public class KafkaSimpleController {
      */
 
     @PostMapping("/v2")
-    public void post(@RequestBody MoreSimpleModel moreSimpleModel){
-        kafkaTemplate.send("myTopic2", jsonConverter.toJson(moreSimpleModel));
+    public ResponseEntity<String> post(@RequestBody MoreSimpleModel moreSimpleModel) throws InterruptedException, ExecutionException{
+        CompletableFuture<SendResult<String, String>> result = kafkaTemplate.send("myTopic2", jsonConverter.toJson(moreSimpleModel));
+        return new ResponseEntity<>(result.get().getProducerRecord().value(), HttpStatus.OK);
     }
 
     @KafkaListener(topics = "myTopic2")
-
-    public void getFromKafka2(String moreSimpleModel){
-        System.out.println(moreSimpleModel);
+    public void getFromKafka2(String moreSimpleModel) {
+        System.out.println("Kafka event consumed is: " + moreSimpleModel);
         MoreSimpleModel simpleModel1 = jsonConverter.fromJson(moreSimpleModel, MoreSimpleModel.class);
-        System.out.println(simpleModel1.toString());
+        System.out.println("Model converted value: " +  simpleModel1.toString());
     }
 }
